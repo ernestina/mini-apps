98ad29b7ae519cb4f7037866648da9203bbb5e52
ernestina <er.ernestina@gmail.com>
2017-08-07 09:12:22 +0700

    initial commit

---
+++ application/controllers/Examples.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Examples extends CI_Controller {
+	private $data;
+	
+	public function __construct(){
+		parent::__construct();
+		$this->load->database();
+		$this->load->helper('url');
+	}
+	
+	public function index(){
+		$this->load->model('example_model'); 
+		$this->data['record'] = $this->example_model->get_profil();
+		//$this->load->view('header_view', $this->data);
+		$this->load->view('example_view', $this->data);
+		//$this->load->view('footer_view');
+	}
+	
+	public function tes(){
+		$this->load->model('example_model'); 
+		$this->data['record'] = $this->example_model->get_profil('KANWIL ACEH');
+		//$this->load->view('header_view', $this->data);
+		$this->load->view('example_view', $this->data);
+		//$this->load->view('footer_view');
+	}	
+	
+}
